7d1138c91d89ccb325f19d1fa90aacd4319881b4
ajkelso <ajkelso@gmail.com>
2021-03-29 13:23:18 -0400

    build out jwt auth

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+    
+    skip_before_action :authorized, only: [:create]
+    
     def index
         users = User.all
         render json: users
     end
 
     def create
+        byebug
         @user = User.create(user_params)
         if @user.valid?
-            render json: { user: @user }, status: :created
+            @token = encode_token(user_id: @user.id)
+            render json: { user: @user, jwt: @token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end

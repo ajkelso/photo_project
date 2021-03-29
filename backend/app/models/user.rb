acc0b50526996d860df8665db452684b59a0c926
ajkelso <ajkelso@gmail.com>
2021-03-28 21:28:23 -0400

    add relationships

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     validates :email, uniqueness: true
+    has_many :galleries
+    has_many :photos, through: :galleries
 end

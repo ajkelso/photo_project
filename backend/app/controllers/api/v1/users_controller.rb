be2c59e4857ce42fdfc368d27243f89fa7af361b
ajkelso <ajkelso@gmail.com>
2021-03-29 21:50:51 -0400

    adjust json to send serialized user data

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -12,14 +12,14 @@ class Api::V1::UsersController < ApplicationController
         @user = User.create(user_params)
         if @user.valid?
             @token = encode_token(user_id: @user.id)
-            render json: { user: @user, jwt: @token }, status: :created
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
     end
 
     def profile
-        render json: { user: current_user }, status: :accepted
+        render json: { user: UserSerializer.new(@user) } , status: :accepted
     end
 
     #update?

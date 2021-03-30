0162779cbc36fac8c350d98dc7052021ef67bed0
ajkelso <ajkelso@gmail.com>
2021-03-29 21:02:45 -0400

    add failed login message and store jwt in localStorage

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,14 +13,23 @@ function Login() {
     const handleSubmit = (e) => {
         e.preventDefault()
         loginRequest({user:{username, password}})
-        .then(console.log)
+        .then(res => {
+            if (res.message) {
+                setMessage(res.message)
+            } else {
+                localStorage.setItem('jwt', res.jwt)
+            }
+        })
     }
 
     return(
         <div>
+            <p>{localStorage.getItem('jwt')}</p>
+
             <p>Login Below </p>
 
             <form onSubmit={handleSubmit}>
+                <p style={{color: 'red'}}>{message}</p>
                 <label htmlFor="username">Username: </label>
                 <input type="text" onChange={handleChangeUsername} value={username}/>
                 <label htmlFor="password">Password: </label>
